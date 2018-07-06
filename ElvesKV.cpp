6a55691dfdffb225e1b1ba4d44042a2862b4b1dc
池桂宇 <1092369395@qq.com>
2018-07-06 13:54:44 +0800

    完成leveldb相关

--- ElvesKV.cpp
+++ ElvesKV.cpp
@@ -16,19 +16,16 @@ static bool leveldbGet(leveldb::DB * db, string &key, string &value){
         return true;
     }
 }
-
 // leveldb 写操作封装 暂时采用PUT方法
 static void leveldbSet(leveldb::DB * db, string key, string value){
     leveldb::Status s = db->Put(leveldb::WriteOptions(),key,value);
     assert(s.ok());
 }
-
 // level 删除操作方法封装
 static void leveldbDel(leveldb::DB * db,string &key){
     leveldb::Status s = db->Delete(leveldb::WriteOptions(),key);
     assert(s.ok());
 }
-
 // leveldb 建库操作封装
 static leveldb::DB * open_leveldb(const string &dirname){
     leveldb::DB * db;
@@ -43,15 +40,44 @@ static void testingLevelDBFunction(leveldb::DB * db, string key, string value){
     string get_value;
     leveldbSet(db,key,value);
     leveldbGet(db,key,get_value);
-    cout<<get_value<<endl;
+    // cout<<get_value<<endl;
     leveldbDel(db,key);
     assert(leveldbGet(db,key,get_value)==0);
 }
-int main(){
+// 生成字符串的长度
+static size_t randValueSize(){
+    return (size_t)rand()/100000 + 5000;
+}
+// 生成随机字符串
+static string randString(){
+    const size_t value_size = randValueSize();
+    // cout<<value_size<<endl;
+    char * vbuf = new char[value_size];
+    for(size_t i=0;i<value_size;i++){
+        vbuf[i]=rand();
+    }
+    string value = string(vbuf,value_size);
+    return value;
+}
+static void levelDbTest(){
     leveldb::DB * db = open_leveldb("/tmp/testdb");
-    string key = "A";
-    string value = "hello world elveskv";
-    string get_value;
-    testingLevelDBFunction(db,key,value);
+    // string value = "hello world elveskv";
+    for(int i=0;i<1000;i++){
+        string key = to_string((size_t)rand());
+        string value = randString();
+        testingLevelDBFunction(db,key,value);
+    }
+}
+int main(){
+    // leveldb::DB * db = open_leveldb("/tmp/testdb");
+    // string key = "A";
+    // string value = "hello world elveskv";
+    // string get_value;
+    // testingLevelDBFunction(db,key,value);
+    // cout << "time elapsed: " << levelDbTest() * 1.0e-6 << " seconds" << endl;
+    clock_t t0 = clock();
+    levelDbTest();
+    clock_t dt = clock() - t0;
+    cout << "time elapsed: " << dt * 1.0e-6 << " seconds" << endl;
     return 0;
 }
